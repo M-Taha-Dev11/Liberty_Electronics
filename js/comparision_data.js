0f89a75272f8f940aea15bac6bd2a0c23e236da0
M-Taha-Dev11 <tahakingshaikh123@gmail.com>
2025-12-26 00:09:26 +0500

    Add files via upload

---
+++ js/comparision_data.js
@@ -0,0 +1,119 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Samsung Galaxy S22",
+    "price": "$259.99",
+    
+    "description": "Samsung Galaxy S22 price in Pakistan is Rs. 179,999. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
+    "brand": "Samsung ",
+    "image": "https://i0.wp.com/www.brandsmerkez.com/wp-content/uploads/2022/08/SamsungGalaxyS22-1.jpg?ssl=1"
+}, {
+    "id": 2,
+    "title": "iphone 13 Pro Max",
+    "price": "$519.00",
+    "description": "Super Retina XDR display with ProMotion for a faster, more responsive feel. Lightning-fast A15 Bionic chip. Durable design and the best battery life ever in an iPhone.",
+    "brand": "Apple",
+    "image": "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone13ProMax-b.jpg"
+}, {
+    "id": 3,
+    "title": "Iphone 14 Pro Max",
+    "price": "$744.99",
+    "description": "The iPhone 14 and iPhone 14 Plus feature a 6.1-inch (15 cm) and 6.7-inch (17 cm) display, improvements to the rear-facing camera",
+    "brand": "Apple",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLB8kn4ITgL1_boBHdWWRiMIvlMO8HHqybbQ&s"
+}, {
+    "id": 4,
+    "title": "Iphone 11 Pro Max",
+    "price": "$205.99",
+    "description": "Apple iPhone 11 Pro Max is powered by the Apple A13 Bionic processor. The smartphone comes with a 6.5 inches Super Retina XDR OLED capacitive touchscreen and 1242 x 2688 pixels resolution. ",
+    "brand": "Apple",
+    "image": "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone11ProMax-b.jpg"
+}, {
+    "id": 5,
+    "title": "Iphone 12 Pro Max",
+    "price": "$459.00",
+    "description": "Samsung Galaxy A13 price in Pakistan is Rs. 35,499. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
+    "brand": "Apple",
+    "image": "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone12Pro-b.jpg"
+}, {
+    "id": 6,
+    "title": "Samsung Galaxy A55",
+    "price": "$445.00",
+    "description": "Samsung Galaxy A55 5G comes with 6.6-inch AMOLED display with 120Hz refresh rate and 5000mAh battery.",
+    "brand": "Samsung",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPHAGtGvZvkRo68pOr_JkOkeZpcBT6VSkGvA&s"
+}, {
+    "id": 7,
+    "title": "Samsung Galaxy Z Fold6",
+    "price": "$1,519.99",
+    "description": "The Samsung Galaxy Z Fold 6 comes with 7.3-ich Dynamic AMOLED display and 6.3-inch display when closed. The specs also include Qualcomm Snapdragon 8 Gen 3 processor, 4400mAh battery and Triple camera setup on the back.",
+    "brand": "Samsung",
+    "image": "https://res.cloudinary.com/mozillion/image/upload/f_auto,q_auto/v1723557546/uhlqpz9lpnruzzpngxfp.png"
+}, {
+    "id": 8,
+    "title": "Samsung Galaxy S23 Ultra",
+    "price": "$1149.99",
+    "description": "The Samsung Galaxy S23 Ultra is the headliner of the S23 series. Specifications are top-notch including 6.8-inch Dynamic AMOLED display with 120Hz refresh rate, Snapdragon 8 Gen 2 processor, 5000mAh battery",
+    "brand": "Samsung",
+    "image": "https://digitalisland.co.nz/wp-content/uploads/2023/04/S23Ultra.png"
+}, {
+    "id": 9,
+    "title": "Samsung S24 Ultra",
+    "price": "$1510.41",
+    "description": "The Samsung Galaxy S24 Ultra comes with 6.8-inch Dynamic AMOLED display with 120Hz refresh rate and Qualcomm Snapdragon 8 Gen 3 processor. Specs also include 5000mAh battery and Quad camera setup on the back.",
+    "brand": "Samsung",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjTHlDKVSHZzlYqNgsJWRQ2ncr1zOtDz-E4A&s"
+}, {
+    "id": 10,
+    "title": "OPPO Reno12 F 5G",
+    "price": "$556.99",
+    "description": "The Oppo Reno2 F offers a 6.53-inch AMOLED display, Helio P70 chipset with 8GB RAM and 128GB storage, and 4000mAh battery. There is a quad-camera setup on the back with a 48MP main sensor, as well as a 16MP pop-up selfie camera.",
+    "brand": "Oppo",
+    "image": "https://www.whatmobile.com.pk/admin/images/Oppo/OppoReno12F5G-b.jpg"
+}, {
+    "id":11,
+    "title": "OPPO A3x",
+    "price": "$121.52.",
+    "description": "6.67-inch IPS LCD touchscreen that delivers a high resolution of 720 x 1604 pixels with a 120 Hz refresh rate.",
+    "brand": "Oppo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScykGkdT-JbCd1MSl6dMaub_yD0n_IeCMDIA&s"
+},{
+    "id":12,
+    "title": "OPPO A60",
+    "price": "$190.97",
+    "description": "Size and Weight. Height. 165.71mm · Storage. RAM and ROM Capacities. 8GB + 256GB · Display. Size. 6.67 inches (diagonal) · Camera. Rear. 50 MP Main camera",
+    "brand": "Oppo",
+    "image": "https://bsimg.nl/images/oppo-a60-256gb-blauw_1.png/w4qd3GUtOAk9We-axO-k3zdIZuM%3D/fit-in/0x0/filters%3Aupscale%28%29" 
+},{
+    "id":13,
+    "title": "OPPO A78",
+    "price": "$565.45",
+    "description": "Oppo A78 Android smartphone. Announced Jan 2023. Features 6.56″ display, Dimensity 700 chipset, 5000 mAh battery, 128 GB storage, 8 GB RAM, Panda glass.",
+    "brand": "Oppo",
+    "image": "https://www.oppo.com/content/dam/oppo/common/mkt/v2-2/a78-en/listpage/a78-427_600-green.png" 
+},{
+   "id":14,
+    "title": "Huawei Mate 70 Pro",
+    "price": "$1,434.99",
+    
+    "description": "The Huawei Mate 70 Pro comes with 6.9-inch OLED display with 120Hz refresh rate and Triple camera setup on the back.",
+    "brand": "Huawei",
+    "image": "https://cdn.phonebunch.com/news-images/2024/11/Huawei-Mate-70-RS-Ultimate-design-1-cn.webp" 
+},{
+    "id":15,
+    "title": "Huawei nova 13 Pro",
+    "price": "$682.00",
+    "description": "Huawei nova 13 Pro smartphone. Announced Oct 2024. Features 6.76″ display, 5000 mAh battery, 1024 GB storage, Aluminosilicate glass.",
+    "brand": "Huawei",
+    "image": "https://khaleejitech.com/wp-content/uploads/2024/10/Huawei-nova-13-Pro-main.jpg"
+}
+
+
+
+]
